86f3f3c733fe239b0c3ca7db91bc0078b370dc01
Fabiana Araujo <fabiana2611@hotmail.com>
2020-10-12 18:11:14 +0100

    merge

--- src/main/java/javanet/MyInetAddress.java
+++ src/main/java/javanet/MyInetAddress.java
@@ -8,24 +8,18 @@ import java.net.UnknownHostException;
  */
 public class MyInetAddress {
 	
-	public void loadInetAddress() {
+	public void loadInetAddressByName() {
 		try {
-			InetAddress address = InetAddress.getByName("www.google.com");
+			InetAddress address = InetAddress.getByName("www.oreilly.com");
 			System.out.println(address);
-			
 			address = InetAddress.getByName("23.216.99.61");
 			System.out.println(address.getHostName());
-			
-			address = InetAddress.getLocalHost();
-			System.out.println(address);
-			System.out.println(address.getHostName());
-			System.out.println(address.getHostAddress());
-			
 		} catch (UnknownHostException ex) {
-			System.out.println("Could not find www.google.com");
+			System.out.println("Could not find www.oreilly.com");
 		}
+
 	}
-	
+
 	public void loadInetAddressArray() {
 		try {
 			InetAddress[] addresses = InetAddress.getAllByName("www.oreilly.com");
@@ -37,5 +31,14 @@ public class MyInetAddress {
 		}
 	}
 
-
+	public void loadLocalHost() {
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			System.out.println(address);
+			System.out.println(address.getHostName());
+			System.out.println(address.getHostAddress());
+		} catch (UnknownHostException ex) {
+			System.out.println("Could not find www.oreilly.com");
+		}
+	}
 }
